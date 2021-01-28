cb13d5d6a3fdf0173d75fe6ab27bc133237e745f
galaxy-s10 <2274751790@qq.com>
2021-01-28 18:57:14 +0800

    0128

---
+++ src/data.js
@@ -0,0 +1,84 @@
+export default {
+    list: [
+        {
+            src: require('./assets/img/1.jpg'),
+            width: 174,
+            height: 174,
+        },
+        {
+            src: require('./assets/img/2.jpg'),
+            width: 130,
+            height: 173,
+        },
+        {
+            src: require('./assets/img/3.jpg'),
+            width: 321,
+            height: 321,
+        },
+        {
+            src: require('./assets/img/4.jpg'),
+            width: 316,
+            height: 133,
+        },
+        {
+            src: require('./assets/img/5.jpg'),
+            width: 179,
+            height: 179,
+        },
+        {
+            src: require('./assets/img/6.jpg'),
+            width: 150,
+            height: 150,
+        },
+        {
+            src: require('./assets/img/7.jpg'),
+            width: 201,
+            height: 135,
+        },
+        {
+            src: require('./assets/img/8.jpg'),
+            width: 160,
+            height: 160,
+        },
+        {
+            src: require('./assets/img/9.jpg'),
+            width: 160,
+            height: 160,
+        },
+        {
+            src: require('./assets/img/10.jpg'),
+            width: 216,
+            height: 120,
+        },
+        {
+            src: require('./assets/img/11.jpg'),
+            width: 140,
+            height: 200,
+        },
+        {
+            src: require('./assets/img/12.jpg'),
+            width: 280,
+            height: 280,
+        },
+        {
+            src: require('./assets/img/13.jpg'),
+            width: 175,
+            height: 175,
+        },
+        {
+            src: require('./assets/img/14.jpg'),
+            width: 160,
+            height: 160,
+        },
+        {
+            src: require('./assets/img/15.jpg'),
+            width: 180,
+            height: 180,
+        },
+        {
+            src: require('./assets/img/16.jpg'),
+            width: 160,
+            height: 160,
+        },
+    ]
+}
